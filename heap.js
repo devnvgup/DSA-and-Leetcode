75fb6d7bd1225a0fe0b738e05546f98c48a42a7a
truongnguyenhitachi <truong.truong@hitachivantara.com>
2022-11-14 11:27:58 +0700

    heapfix

--- heap.js
+++ heap.js
@@ -1,77 +1,77 @@
 class MinHeap {
     constructor() {
-        arr = new Array(101)
-        size = 0
-        this.arr = arr
+      this.arr = new Array(11);
+      this.size = 0;
     }
-
     isEmty() {
-        return size == 0
+      return this.size === 0;
     }
-
     peek() {
-        if (this.isEmty()) return false
-        return arr[1]
+      if (!this.isEmty()) return;
+      return this.arr[1];
     }
-
     add(node) {
-        arr[size + 1] = node
-        let curIndex = this.size
-        let parenIndex = curIndex / 2
-        while (parenIndex !== 0 && arr[parenIndex] > arr[curIndex]) {
-            this.swap(parenIndex, curIndex)
-            curIndex = parenIndex
-            parenIndex = curIndex / 2
-        }
-    }
-    swap(i, j) {
-        let tmp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = tmp
+      this.arr[this.size + 1] = node;
+      this.size++;
+      let curIndex = this.size;
+      let parentIndex = Math.floor(curIndex / 2);
+      while (parentIndex !== 0 && this.arr[parentIndex] > this.arr[curIndex]) {
+        this.swap(parentIndex, curIndex);
+        curIndex = parentIndex;
+        parentIndex = Math.floor(curIndex / 2);
+      }
     }
     poll() {
-        if (this.isEmty()) return false
-        let minRoot = arr[1]
-        arr[1] = arr[size]
-        size--
-        //heapify-down
-        this.headifyDown(1)
-        return minRoot
+      this.headifyDown(1)
     }
-    remove(node) {
-        curIndex = -1
-        for (let i = 0; i <= size; i++) {
-            if (arr[i] == node) {
-                curIndex = i
-                break
-            }
+    remove(node){
+      let curIndex = -1
+      for(let i = 0;i< this.arr.length;i++){
+        if(this.arr[i]===node){
+          curIndex = i
         }
-        if (curIndex = -1) {
-            return
-        }
-
-        arr[curIndex] = arr[size]
-        size--
-        //heapify
-        this.headifyDown(curIndex)
-        return node
+      }
+      if(curIndex === -1) return false
+      this.headifyDown(curIndex)
     }
-    
+    swap(i, j) {
+      let tmp = this.arr[i];
+      this.arr[i] = this.arr[j];
+      this.arr[j] = tmp;
+    }
     headifyDown(index){
-        let curIndex = index
-        while ((2 * curIndex) <= size) {
-            let leftIndex = 2 * curIndex
-            let smallerIndex = leftIndex
-            let rightIndex = leftIndex + 1
-            if (rightIndex <= size && arr[rightIndex] < arr[leftIndex]) {
-                smallerIndex = rightIndex
-            }
-            if (arr[curIndex] > arr[smallerIndex]) {
-                this.swap(curIndex, smallerIndex)
-                curIndex = smallerIndex
-            } else {
-                break
-            }
+      let curIndex = index;
+      this.arr[index] = this.arr[this.size];
+      while(this.arr[curIndex]){
+        let leftChildIndex = 2 * curIndex;
+        let smallerChild = leftChildIndex;
+        let rightChildIndex = leftChildIndex + 1;
+        if (
+          rightChildIndex <= this.size &&
+          this.arr[rightChildIndex] < this.arr[leftChildIndex]
+        ) {
+          smallerChild = rightChildIndex;
         }
+        if (this.arr[smallerChild] < this.arr[curIndex]) {
+          this.swap(smallerChild, curIndex);
+          curIndex = smallerChild;
+        }else {
+          this.arr[this.size] = null;
+          this.size--;
+          return
+        }
+      }
     }
-}
+  }
+  
+  let minHeap = new MinHeap();
+  minHeap.add(10);
+  minHeap.add(20);
+  minHeap.add(50);
+  minHeap.add(30);
+  minHeap.add(5);
+  minHeap.poll();
+  minHeap.remove(50)
+  
+  console.log(minHeap);
+  
